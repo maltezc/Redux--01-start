8d1f2916724e874689dc35493c97a1e5addaeac1
cflux1990 <cflux.maltez@live.com>
2018-09-21 07:54:11 -0700

    14.244 - Adding Subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -29,6 +29,10 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState())
+})
 
 
 // Dispatching Action
@@ -37,4 +41,5 @@ store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState())
 
-// Subscription
+
+
