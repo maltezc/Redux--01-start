78169e38b75e6c269baec31024d2b41808a47a65
cflux1990 <cflux.maltez@live.com>
2018-09-21 07:51:17 -0700

    14.243 - Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -8,6 +8,18 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 };
 
@@ -20,5 +32,9 @@ console.log(store.getState());
 
 
 // Dispatching Action
+
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState())
 
 // Subscription
